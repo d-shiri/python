9e6d55d36356b957f90a73387af7d1dd215e80c4
Dariyoush Shiri <44789851+d-shiri@users.noreply.github.com>
2023-12-11 13:36:12 +0100

    Update progress_bar.py

--- progress_bar/progress_bar.py
+++ progress_bar/progress_bar.py
@@ -9,31 +9,29 @@ Created on Tue Aug 9 | 23:58:44 2022
 import os
 import time
 
-# Get the resolution of the terminal window
-row, _ = os.get_terminal_size()
 
-def progress(progress, n, count=[0]) -> None:
+def progress(i, n) -> None:
     '''
     Simple progress bar that adjust to the terminal window
 
     Parameters
     ----------
-    progress : int
+    i : int
         single steps that program takes (e.g., in a for loop)
     n : int
         Total number of steps
-    count : list, optional
-        by default [0]
     '''
-    
-    count[0] += 1
-    perc = (count[0] *(100 / float(n)))
-    prog = int((perc*(row/1.5))/100)
-    bar = '\033[31m\033[01m{}\033[0m'.format('━'*prog) + '┄'* int((row/1.5)-prog)
-    print(f"\r{bar} {perc:.2f}%", end='\r')
+    row, _ = os.get_terminal_size()
+    row = row - 8
+    percent = (i +1) / n
+    done = int(percent * row)
+    remain = int(row-done)
+    #print(f"Remaining: {remain} | Done: {done}", end='\r')
+    bar = '\033[31m\033[01m{}\033[0m'.format('━'*done) + '┄'*remain
+    print(f"\r{bar}{percent*100:.2f}%", end='\r')
 
 #NOTE: Example
-for i in range(100, 222):
+for i in range(1, 20):
     time.sleep(.1)
-    progress(i, 122)
+    progress(i, 20)
 print()
